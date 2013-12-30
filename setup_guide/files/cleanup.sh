e373018cf7ddc26c3b28ac6c1ef34642481c0277
kugurst <ma2799@gmail.com>
2013-12-30 17:17:39 -0500

    Still working on step 2.
    Going over how to apply the patches.

---
+++ setup_guide/files/cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+rm -f modules/linux/vmci/Makefile modules/linux/vmci/common/vmciDatagram.c \
+modules/linux/vmci/common/vmciDoorbell.h modules/linux/vmci/common/vmciDriver.h \
+modules/linux/vmci/common/vmciHashtable.h modules/linux/vmci/common/vmciQPair.c \
+modules/linux/vmci/common/vmciQueuePair.c \
+modules/linux/vmci/common/vmciResource.h \
+modules/linux/vmci/common/vmciRoute.c modules/linux/vmci/common/vmciRoute.h \
+modules/linux/vmci/linux/driver.c modules/linux/vmci/linux/vmciKernelIf.c \
+modules/linux/vmci/linux/vmci_version.h modules/linux/vmci/shared/pgtbl.h \
+modules/linux/dkms.conf.rej modules/linux/vmci/common/vmciCommonInt.h \
+modules/linux/vmci/common/vmciContext.c modules/linux/vmci/common/vmciContext.h \
+modules/linux/vmci/common/vmciDatagram.h \
+modules/linux/vmci/common/vmciDoorbell.c modules/linux/vmci/common/vmciDriver.c \
+modules/linux/vmci/common/vmciEvent.c modules/linux/vmci/common/vmciEvent.h \
+modules/linux/vmci/common/vmciHashtable.c \
+modules/linux/vmci/common/vmciQueuePair.h \
+modules/linux/vmci/common/vmciResource.c modules/linux/vmci/COPYING \
+modules/linux/vmci/Makefile.kernel modules/linux/vmci/Makefile.normal \
+modules/linux/vmci/Makefile.rej modules/linux/vmci/shared/vmci_handle_array.h \
+modules/linux/vmci/shared/vmci_page_channel.h \
+modules/linux/vmci/shared/vmciQueue.h
